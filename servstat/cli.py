a72f4af98cb108a9905f58e9ed2b14484c0ba308
bloomg <greig.bloom@ipsoft.com>
2024-02-22 17:15:31 +0900

    Add option: show failed sites.

--- servstat/cli.py
+++ servstat/cli.py
@@ -3,6 +3,7 @@ from requests.exceptions import MissingSchema
 import click
 import pandas as pd
 from bs4 import BeautifulSoup
+import webbrowser
 import warnings
 from importlib import metadata
 warnings.filterwarnings("ignore")
@@ -11,12 +12,17 @@ warnings.filterwarnings("ignore")
 def check_status_page(site):
     pass
 
-def print_status_page(site):
+def print_status_page(site, show_failed_site=False):
     output = site.Service + ": "#.rjust(20)
     try:
         resp = requests.get(site.URL)
         soup = BeautifulSoup(resp.content)
-        output = output.ljust(18) + (click.style(" ●", fg="green") if site.NormalStatus.lower() in str(soup).lower() else click.style(" X", fg="red"))
+        if site.NormalStatus.lower() in str(soup).lower():
+            output = output.ljust(18) + click.style(" ●", fg="green") 
+        else:
+            output = output.ljust(18) + click.style(" X", fg="red")
+            if show_failed_site:
+                click.launch(site.URL)
     except (AttributeError, MissingSchema) as e:
         output = output.ljust(18) + click.style("▲", fg="yellow")
     click.echo(output)
@@ -26,9 +32,10 @@ def print_status_page(site):
 @click.version_option(metadata.version("servstat"))
 @click.command()
 @click.option("--file")
+@click.option("--show_failed_site", is_flag=True)
 @click.option("--json_format", is_flag=True)
 @click.option("--export_csv", is_flag=True)
-def cli(file, json_format, export_csv):
+def cli(file, json_format: bool, export_csv:bool, show_failed_site:bool):
     print("Checking system status for various sites")
     df = pd.read_csv(file)  
     for site in list(df.iterrows()):
@@ -40,7 +47,7 @@ def cli(file, json_format, export_csv):
             continue
 
         #TODO: Refactor to separate CHECK and PRINT (abstract into OUTPUT)
-        print_status_page(site[1])
+        print_status_page(site[1], show_failed_site)
 
 
 if __name__ == "__main__":
